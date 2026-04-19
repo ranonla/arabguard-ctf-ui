358de545a6b3ccebc3adfc74f23c3913dd4e3c5f
ranonla <112718385+ranonla@users.noreply.github.com>
2026-04-19 13:36:47 +0200

    add achievements data with icons

---
+++ src/constants/achievementsData.ts
@@ -0,0 +1,39 @@
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import CodeIcon from "@mui/icons-material/Code";
+import SecurityIcon from "@mui/icons-material/Security";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import CloudIcon from "@mui/icons-material/Cloud";
+import TerminalIcon from "@mui/icons-material/Terminal";
+
+export const achievementsData = [
+    {
+      title: "First Breach",
+      icon: LockOpenIcon,
+      unlocked: true,
+    },
+    {
+      title: "Code Breaker",
+      icon: CodeIcon,
+      unlocked: true,
+    },
+    {
+      title: "Firewall Slayer",
+      icon: SecurityIcon,
+      unlocked: false,
+    },
+    {
+      title: "Silent Intruder",
+      icon: VisibilityOffIcon,
+      unlocked: false,
+    },
+    {
+      title: "Data Phantom",
+      icon: CloudIcon,
+      unlocked: false,
+    },
+    {
+      title: "Root Access",
+      icon: TerminalIcon,
+      unlocked: false,
+    },
+  ];
