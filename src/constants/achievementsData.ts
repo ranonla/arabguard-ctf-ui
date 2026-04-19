f15ad5da0f97dcf92e364466519ecf36839bd9e1
ranonla <112718385+ranonla@users.noreply.github.com>
2026-04-19 17:35:03 +0200

    add ids to achievements data

--- src/constants/achievementsData.ts
+++ src/constants/achievementsData.ts
@@ -7,31 +7,37 @@ import TerminalIcon from "@mui/icons-material/Terminal";
 
 export const achievementsData = [
     {
+      id: 1,
       title: "First Breach",
       icon: LockOpenIcon,
       unlocked: true,
     },
     {
+      id: 2,
       title: "Code Breaker",
       icon: CodeIcon,
       unlocked: true,
     },
     {
+      id: 3,
       title: "Firewall Slayer",
       icon: SecurityIcon,
       unlocked: false,
     },
     {
+      id: 4,
       title: "Silent Intruder",
       icon: VisibilityOffIcon,
       unlocked: false,
     },
     {
+      id: 5,
       title: "Data Phantom",
       icon: CloudIcon,
       unlocked: false,
     },
     {
+      id: 6,
       title: "Root Access",
       icon: TerminalIcon,
       unlocked: false,
