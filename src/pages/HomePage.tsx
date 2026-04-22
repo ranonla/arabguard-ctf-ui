b8a5077e1c9a9fde8dd6bb719af482959d187c9d
ranonla <112718385+ranonla@users.noreply.github.com>
2026-04-23 00:47:29 +0200

    remove unnecessary wrapper in Home component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import Navbar from '../components/home/Navbar';
 import GuardianHero from '../components/home/GuardianHero';
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <GuardianHero />
-    </div>
+    </>
   )
 }
 
