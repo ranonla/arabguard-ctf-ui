79eabb066ae1f1e1b135ad927f718597db45f7f2
ranonla <112718385+ranonla@users.noreply.github.com>
2026-06-29 21:42:15 +0300

    add routes for admin panel and character management

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Home, Dashboard, ChatbotPage }  from './pages';
 import {ChatToaster} from './components/chat';
+import AdminPanel from './pages/AdminPanel';
+import { DisplayCharacters, AddCharacter } from './components/Admin';
 
 function App() {
 
@@ -10,7 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/adminPanel" element={<AdminPanel />} />
         <Route path="/chatbot/:chatbotId" element={<ChatbotPage />} />
+        <Route path="/admin/characters" element={<DisplayCharacters />} />
+        <Route path="/admin/character/new" element={<AddCharacter />} />
       </Routes>
     </BrowserRouter>
 
